bf3c21c79228d5bc9c3de1b82ac76cad8c6af369
RafaMC97 <rafa.mc97@gmail.com>
2016-05-11 21:12:20 -0300

    Funcao principal funciona

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+bool isPrimo(int n){
+
+    if(n<5 || n%2==0 || n%3==0)
+        return (n==2||n==3);
+
+    int maxP = sqrt(n)+2;
+    for(int p=5; p<maxP; p+=6)
+        if(n%p==0||n%(p+2)==0)
+            return false;
+
+    return true;
+}
+
 int main() {
-    int x;
+    int u,primos=0;
     cout << "---------------------\nContador de Numeros Primos\n---------------------\n\n" << endl;
     cout << "Digite um numero: ";
-    cin >> x;
+    cin >> u;
 
+    for (int x=0;x<u;x++){
+        if(isPrimo(x))
+            primos++;
+    }
 
+    cout << "Numeros primos entre 0 e " << u << " = " << primos << endl;
 
     return 0;
 }
+
